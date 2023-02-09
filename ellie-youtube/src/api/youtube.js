3a5fea128f3ceffc3e64c6e367ee2af202b4f428
kot8585 <ansgywjd321@gmail.com>
2023-02-09 18:00:35 +0900

    "ellie" - 실제 api로 전송하는 youtube 클래스 만들기
    - axios 기본 설정
    - apiKey는 .env 파일에 설정

--- ellie-youtube/src/api/youtube.js
+++ ellie-youtube/src/api/youtube.js
@@ -1,7 +1,43 @@
 import axios from 'axios';
 
-export async function search(keyword) {
-    return axios
-      .get(`/videos/${keyword? 'search' : 'popular'}.json`)
-      .then((res) => res.data.items);
+export default class Youtube {
+  //❗️기본 네트워크 설정 해주기 
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: {key: process.env.REACT_APP_YOUTUBE_API_KEY}
+    })
   }
+
+  async search(keyword) {
+    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+  }
+
+  //❗️파라미터 세팅해주기
+  async #searchByKeyword(keyword) {
+    return this.httpClient
+    .get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        type: 'video',
+        q: keyword,
+      }
+    })
+    .then((response) => response.data.items)
+    .then((items) => items.map((item) => ({...item, id: item.id.videoId})))
+  }
+
+   async #mostPopular() {
+    return this.httpClient
+    .get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        chart: 'mostPopular',
+      }
+    })
+    .then((response) => response.data.items);
+  }
+
+}
