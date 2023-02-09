2dc88ee39b97528a7392a70638a0eb9cd679fb0a
kot8585 <ansgywjd321@gmail.com>
2023-02-09 12:47:44 +0900

    "lecture" - url에 search keyword가 있을경우 param을 가져와서 search에 세팅시킴

--- my-youtube/src/pages/Header.jsx
+++ my-youtube/src/pages/Header.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {BsYoutube, BsSearch} from 'react-icons/bs'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function Header() {
-  const [search, setSearch] = useState('');
+  const {search} = useParams();
+  const [text, setText] = useState('');
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -12,15 +13,15 @@ export default function Header() {
   }
 
   const handleChange = (e) => {
-    setSearch(e.target.value);
+    setText(e.target.value);
   } 
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //버튼 눌리면 react-router로 페이지만 이동시키고 search페이지에서 뿌려주기
-    setSearch('');
-    navigate(`/videos/${search}`);
+    navigate(`/videos/${text}`);
   }
+
+  useEffect(() => setText(search ? search : ''), [search]);
 
   return (
     <header className='sticky top-0 py-3 align-middle  border-b-2 bg-bgPrimary'>
@@ -31,7 +32,7 @@ export default function Header() {
       </button>
       {/* 얘를 어떻게 가운데로 보내지? 얘만 flex로 주는거 맞나?*/}
       <form onSubmit={handleSubmit} className='flex items-center justify-center'>
-        <input type='text' value={search} onChange={handleChange} placeholder='Search..' className='border p-1 h-9 w-80'/>
+        <input type='text' value={text} onChange={handleChange} placeholder='Search..' className='border p-1 h-9 w-80'/>
         <button type='submit' className='border h-9 w-9 text-center'><BsSearch className='inline-block align-baseline'/></button>
       </form>
     </header>
