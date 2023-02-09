666a2bc8bfe0e11ff6615bbf8d337b0817387eb7
kot8585 <ansgywjd321@gmail.com>
2023-02-09 19:44:57 +0900

    "ellie" - client를 전역으로 설정
    - ts의 interface 사용하면 fakeYoutube.js랑 youtube.js랑 동일한 규격을 따르게 할 수 있다,,?
    - 여기서 한번 더 리팩토링을 할 수 있대!

--- ellie-youtube/src/pages/Videos.jsx
+++ ellie-youtube/src/pages/Videos.jsx
@@ -1,16 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import FakeYoutube from '../api/fakeYoutube';
-import Youtube, { search } from '../api/youtube';
 import VideoCard from '../components/VideoCard';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
+  const youtube = useYoutubeApi();
   const {keyword} = useParams();
   // ❓ 키워드별로 캐시..... 그러면 캐시가 너무 많이 되는거  아냐??
   const {isLoading, error, data: videos} = useQuery( ['videos', keyword], () => {
-    const youtube =  new Youtube();
-    return  youtube.search(keyword);
+    return youtube.search(keyword);
   });
 
 
